50051fa1212eac158b6afe7ba99badf619e7f17b
YurySubbota <plus375291421728@gmail.com>
2024-03-06 10:17:53 +0300

    homework

--- homeworks/django2/blog/urls.py
+++ homeworks/django2/blog/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.post_iist, name='post_list'),
     path('post/<int:post_id>/', views.post_detail, name='post'),
     path('create_post/', views.create_post, name='create_post'),
-    path('tags/<tag_slug>/', views.posts_by_tag, name='posts_by_tag')
+    path('tags/<tag_slug>/', views.posts_by_tag, name='posts_by_tag'),
+    path('author/<author_slug>/', views.posts_by_author, name='posts_by_author'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post')
 
 ]
--- homeworks/django2/blog/views.py
+++ homeworks/django2/blog/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from blog.forms import PostForm
 from taggit.models import Tag
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -23,17 +25,20 @@ def post_iist(request):
 
 
 def post_detail(request, post_id):
+    current_user = request.user
     post = get_object_or_404(Post, id=post_id)
-    return render(request, 'post_detail.html', {'post': post})
+    return render(request, 'post_detail.html', {'post': post, 'current_user': current_user})
 
 
+@login_required(login_url='login')
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.save()
+            print(post)
+            post.update()
             form.save_m2m()
             return redirect('post_list')
         else:
@@ -47,3 +52,30 @@ def posts_by_tag(request, tag_slug):
     tag = Tag.objects.get(slug=tag_slug)
     posts = Post.objects.filter(tagged_items__tag_id__in=[tag.id])
     return render(request, 'posts_by_tag.html', {"posts": posts, 'tag': tag})
+
+
+def posts_by_author(request, author_slug):
+    author = User.objects.get(username=author_slug)
+    posts = Post.objects.filter(author=author)
+    return render(request, 'post_by_author.html', {'posts': posts, 'author': author})
+
+
+@login_required(login_url='login')
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        return redirect(f'/post/{post_id}/')
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.id = post_id
+            post.author = request.user
+            post.save()
+            form.save_m2m()
+            return redirect(f'/post/{post_id}/')
+        else:
+            return redirect(f'/post/{post_id}/')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'edit_post.html', {'form': form, 'post': post})
